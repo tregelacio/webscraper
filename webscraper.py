29ec2e45671cded78c4c2d876104f881df868c18
tregelacio <tre.gelacio@gmail.com>
2019-05-26 13:43:29 -1000

    import from sublime
    Starting code for webscraper

---
+++ webscraper.py
@@ -0,0 +1,48 @@
+import requests
+import urllib
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = 'https://stockx.com/nike-ld-waffle-sacai-blue-multi'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+print ('before find')
+
+print soup.findAll('h3', limit=6)
+
+print ('found all')
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+wait = WebDriverWait(driver, 10)
+
+elem = driver.find_element_by_class_name('form_group')
+
+
+#button = driver.find_element_by_class_name('form-group')
+#button.click()
+
+''' This prints out the info on the starting page. No info of other sizes
+sale = soup.select('.last-sale-block')
+
+for sale in soup.select('.last-sale-block'):
+
+	print sale.text
+
+ask = soup.select('.bid')
+
+for ask in soup.select('.bid'):
+
+	print ask.text
+
+bid = soup.select('.ask')
+
+for bid in soup.select('.ask'):
+
+	print bid.text
+'''
+# print soup
